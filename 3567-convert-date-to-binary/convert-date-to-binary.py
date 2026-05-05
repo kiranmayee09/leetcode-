6ac9371465034bdc02e3ad770a971a66bb8f38e7
kiranmayee09 <kiranmayee0919@gmail.com>
2026-05-05 19:00:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (48.42%) - LeetSync

---
+++ 3567-convert-date-to-binary/convert-date-to-binary.py
@@ -0,0 +1,10 @@
+class Solution:
+    def convertDateToBinary(self, date: str) -> str:
+        
+        year, month, day = date.split('-')
+
+        year_bin = bin(int(year))[2:]
+        month_bin = bin(int(month))[2:]
+        day_bin = bin(int(day))[2:]
+
+        return year_bin + '-' + month_bin + '-' + day_bin
