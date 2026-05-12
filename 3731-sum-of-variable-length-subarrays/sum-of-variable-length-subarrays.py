06f8839416ba8bb1157ab435eb14cb0e6f759c7a
kiranmayee09 <kiranmayee0919@gmail.com>
2026-05-12 19:35:17 +0530

    Time: 2 ms (86.41%) | Memory: 19.2 MB (90.59%) - LeetSync

---
+++ 3731-sum-of-variable-length-subarrays/sum-of-variable-length-subarrays.py
@@ -0,0 +1,18 @@
+class Solution:
+    def subarraySum(self, nums: List[int]) -> int:
+        
+        n = len(nums)
+
+        prefix = [0] * (n + 1)
+
+        for i in range(n):
+            prefix[i + 1] = prefix[i] + nums[i]
+
+        total = 0
+
+        for i in range(n):
+            start = max(0, i - nums[i])
+
+            total += prefix[i + 1] - prefix[start]
+
+        return total
