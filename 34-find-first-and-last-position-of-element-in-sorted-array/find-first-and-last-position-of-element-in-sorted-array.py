87fa8d76e51131ede51abe673c7ff329d0be6dbd
kiranmayee09 <kiranmayee0919@gmail.com>
2026-05-19 00:09:33 +0530

    Time: 0 ms (100.00%) | Memory: 20.5 MB (59.59%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,54 +1,46 @@
 class Solution:
-
-    def lower_bound(self, nums, target):
-
-        low = 0
-        high = len(nums) - 1
-
-        ans = len(nums)
-
-        while low <= high:
-
-            mid = (low + high) // 2
-
-            if nums[mid] >= target:
-
-                ans = mid
-                high = mid - 1
-
-            else:
-                low = mid + 1
-
-        return ans
-
-    def upper_bound(self, nums, target):
-
-        low = 0
-        high = len(nums) - 1
-
-        ans = len(nums)
-
-        while low <= high:
-
-            mid = (low + high) // 2
-
-            if nums[mid] > target:
-
-                ans = mid
-                high = mid - 1
-
-            else:
-                low = mid + 1
-
-        return ans
-
     def searchRange(self, nums, target):
+        first = self.first_occ(nums, target)
 
-        first = self.lower_bound(nums, target)
-
-        if first == len(nums) or nums[first] != target:
+        if first == -1:
             return [-1, -1]
 
-        last = self.upper_bound(nums, target) - 1
+        last = self.last_occ(nums, target)
 
         return [first, last]
+
+    def first_occ(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        ans = -1
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                ans = mid
+                high = mid - 1
+
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans
+
+    def last_occ(self, nums, target):
+        low = 0
+        high = len(nums) - 1
+        ans = -1 
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if nums[mid] == target:
+                ans = mid
+                low = mid + 1
+
+            elif nums[mid] < target:
+                low = mid + 1
+            else:
+                high = mid - 1
+        return ans 
