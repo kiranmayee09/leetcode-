efaec85844d29ecde5f17c3b57afbff9c1615d0c
kiranmayee09 <kiranmayee0919@gmail.com>
2026-07-28 21:04:09 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (56.33%) - LeetSync

--- 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
+++ 2128-reverse-prefix-of-word/reverse-prefix-of-word.py
@@ -1,7 +1,26 @@
 class Solution:
     def reversePrefix(self, word: str, ch: str) -> str:
-        if ch not in word:
+        """ if ch not in word:
             return word
         indexch = word.index(ch)
 
-        return word[indexch::-1] + word[indexch+1::]
+        return word[indexch::-1] + word[indexch+1::] """
+
+        stack = []
+
+        index = word.find(ch)
+
+        if index == -1:
+            return word
+
+        for i in range(index + 1):
+            stack.append(word[i])
+
+        ans = ""
+
+        while stack:
+            ans += stack.pop()
+
+        ans += word[index + 1::]
+
+        return ans
