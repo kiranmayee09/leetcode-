fe1d3effe128913cfa8eef185eeeb9bbdfac76ba
kiranmayee09 <kiranmayee0919@gmail.com>
2026-07-31 00:26:28 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (5.97%) - LeetSync

--- 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
+++ 1570-final-prices-with-a-special-discount-in-a-shop/final-prices-with-a-special-discount-in-a-shop.py
@@ -1,10 +1,24 @@
 class Solution:
     def finalPrices(self, prices: List[int]) -> List[int]:
-        n = len(prices)
-
+        """ n = len(prices)
         for i in range(n):
             for j in range(i+1, n):
                 if prices[j] <= prices[i]:
                     prices[i] = prices[i] - prices[j]
                     break
+        return prices """
+
+        stack = []
+
+        for i in range(len(prices)-1, -1, -1):
+            original = prices[i]
+
+            while stack and stack[-1] > original:
+                stack.pop()
+            
+            if stack:
+                prices[i] = original - stack[-1]
+
+            stack.append(original)
+
         return prices
