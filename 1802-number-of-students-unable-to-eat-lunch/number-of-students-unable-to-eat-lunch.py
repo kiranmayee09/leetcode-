c13689d8ec8c4276392d61e06835af16ea9774c2
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-03 01:16:18 +0530

    Time: 2 ms (14.85%) | Memory: 19.3 MB (65.49%) - LeetSync

--- 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
+++ 1802-number-of-students-unable-to-eat-lunch/number-of-students-unable-to-eat-lunch.py
@@ -1,7 +1,37 @@
+from collections import deque
+
 class Solution:
     def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
         
-        count0 = students.count(0)
+        studentQueue = deque(students)
+        sandwichesStack = sandwiches[::-1]
+
+        sandwichMissed = 0
+
+        while studentQueue:
+            if sandwichesStack[-1] == studentQueue[0]:
+                sandwichesStack.pop()
+                studentQueue.popleft()
+                sandwichMissed = 0
+            else:
+                studentQueue.append(studentQueue.popleft())
+                sandwichMissed += 1
+
+                if sandwichMissed == len(studentQueue):
+                    break
+
+        return len(studentQueue)
+
+
+
+
+
+
+
+
+
+
+        """ count0 = students.count(0)
         count1 = students.count(1)
 
         for s in sandwiches:
@@ -13,4 +43,6 @@ class Solution:
                 if count1 == 0:
                     return count0
                 count1 -= 1
-        return 0
+        return 0 """
+
+        
