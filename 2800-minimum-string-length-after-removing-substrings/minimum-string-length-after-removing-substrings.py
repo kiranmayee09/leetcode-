3c4d779c537c4368004b4502aedf2fbc4c2539c5
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-03 16:38:37 +0530

    Time: 3 ms (83.19%) | Memory: 19.3 MB (52.87%) - LeetSync

--- 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
+++ 2800-minimum-string-length-after-removing-substrings/minimum-string-length-after-removing-substrings.py
@@ -1,7 +1,7 @@
 class Solution:
     def minLength(self, s: str) -> int:
 
-        stack = []
+        """ stack = []
 
         for ch in s:
             stack.append(ch)
@@ -14,6 +14,31 @@ class Solution:
                 elif stack[-2] == "C" and stack[-1] == "D":
                     stack.pop()
                     stack.pop()
-        return len(stack)
+        return len(stack) 
 
+        stack = []
+
+        for ch in s:
+            stack.append(ch)
+
+            if len(stack) >= 2 and (
+                (stack[-2] == "A" and stack[-1] == "B") or
+                (stack[-2] == "C" and stack[-1] == "D")
+            ):
+                stack.pop()
+                stack.pop()
+        return len(stack) """
+
+        stack = []
+
+        for ch in s:
+
+            if stack:
+                if (stack[-1] == "A" and ch == "B") or (stack[-1] == "C" and ch == "D"):
+                    stack.pop()
+                else:
+                    stack.append(ch)
+            else:
+                stack.append(ch)
+        return len(stack)
         
