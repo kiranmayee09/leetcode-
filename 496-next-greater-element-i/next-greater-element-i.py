c23cc6adf14c7e5208e07e3e7a963e417d9ff155
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-05 18:13:22 +0530

    Time: 3 ms (40.20%) | Memory: 19.4 MB (28.62%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,6 +1,6 @@
 class Solution:
     def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        answer = []
+        """ answer = []
 
         for number in nums1:
             found = False
@@ -18,4 +18,34 @@ class Solution:
             if found == False:
                 answer.append(-1)
 
-        return answer
+        return answer """
+
+        """ stack = []
+        next_grater = {}
+
+        for num in nums2:
+            while stack and num > stack[-1]:
+                next_grater[stack.pop()] = num
+            stack.append(num)
+
+        while stack:
+            next_grater[stack.pop()] = -1
+
+        answer = []
+        for num in nums1:
+            answer.append(next_grater[num])
+
+        return answer """
+
+        nums1Idx = { n:i for i, n in enumerate(nums1)}
+        res = [-1] * len(nums1)
+
+        for i in range(len(nums2)):
+            if nums2[i] not in nums1Idx:
+                continue
+            for j in range(i + 1, len(nums2)):
+                if nums2[j] > nums2[i]:
+                    idx = nums1Idx[nums2[i]]
+                    res[idx] = nums2[j]
+                    break
+        return res
