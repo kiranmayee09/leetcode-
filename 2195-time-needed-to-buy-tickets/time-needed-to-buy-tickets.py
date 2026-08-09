d7768975f35d9d2ab1983e29ae83f8921e5181b1
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-10 00:32:45 +0530

    Time: 9 ms (42.49%) | Memory: 19.4 MB (22.93%) - LeetSync

--- 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
+++ 2195-time-needed-to-buy-tickets/time-needed-to-buy-tickets.py
@@ -1,6 +1,6 @@
 class Solution:
     def timeRequiredToBuy(self, tickets: List[int], k: int) -> int:
-        res = 0
+        """ res = 0
 
         for i in range(len(tickets)):
             if i <= k:
@@ -8,4 +8,19 @@ class Solution:
             else:
                 res += min(tickets[i], tickets[k] - 1)
 
-        return res
+        return res """
+
+        q = deque(range(len(tickets)))
+        time = 0
+
+        while q:
+            person = q.popleft()
+
+            tickets[person] -= 1
+            time += 1
+
+            if person == k and tickets[person] == 0:
+                return time
+
+            if tickets[person] > 0:
+                q.append(person)
