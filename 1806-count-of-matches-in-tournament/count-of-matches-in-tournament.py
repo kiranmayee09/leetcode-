f0eb42162735ccbab3035d9bd57d4b5e7424c5c9
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-19 21:20:52 +0530

    Time: 40 ms (82.85%) | Memory: 19.2 MB (19.50%) - LeetSync

--- 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
+++ 1806-count-of-matches-in-tournament/count-of-matches-in-tournament.py
@@ -10,6 +10,13 @@ class Solution:
                 matches += (n - 1) // 2
                 n = n // 2 + 1
 
-        return matches """
+        return matches 
         
-        return n - 1
+        return n - 1 """
+
+        matches = 0
+
+        while n > 1:
+            matches += n // 2
+            n = (n + 1) // 2
+        return matches
