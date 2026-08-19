31c9fbc094663d1dd24a24f71cccb7f57d973f76
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-19 21:07:38 +0530

    Time: 30 ms (26.02%) | Memory: 19.3 MB (13.62%) - LeetSync

--- 2752-sum-multiples/sum-multiples.py
+++ 2752-sum-multiples/sum-multiples.py
@@ -3,11 +3,7 @@ class Solution:
         total = 0
 
         for i in range(1, n + 1):
-            if i % 3 == 0:
-                total = total + i
-            elif i % 5 == 0:
-                total = total + i
-            elif i % 7 == 0:
+            if i % 3 == 0 or i % 5 == 0 or i % 7 == 0:
                 total = total + i
         
         return total
