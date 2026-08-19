121909f40ce3c03e3a15f076a3fdaf238f1d4c6d
kiranmayee09 <kiranmayee0919@gmail.com>
2026-08-19 21:04:22 +0530

    Time: 0 ms (100.00%) | Memory: 19.2 MB (48.84%) - LeetSync

--- 2383-add-two-integers/add-two-integers.py
+++ 2383-add-two-integers/add-two-integers.py
@@ -1,4 +1,12 @@
 class Solution:
     def sum(self, num1: int, num2: int) -> int:
-        ans = num1 + num2
-        return ans
+        """ ans = num1 + num2
+        return ans 
+
+        return num1 + num2
+
+        return sum([num1, num2]) """ 
+
+        return operator.add(num1, num2)
+
+
