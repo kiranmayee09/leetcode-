31e668bea0bf83ecc3030f9b3be48b70c223ed17
kiranmayee09 <kiranmayee0919@gmail.com>
2026-09-07 21:30:20 +0530

    Time: 61 ms (22.53%) | Memory: 19.9 MB (69.56%) - LeetSync

--- 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.py
+++ 4087-maximum-substrings-with-distinct-start/maximum-substrings-with-distinct-start.py
@@ -1,3 +1,10 @@
 class Solution:
     def maxDistinct(self, s: str) -> int:
-        return len(set(s))
+        """ return len(set(s)) """
+
+        seen = set()
+
+        for ch in s:
+            seen.add(ch)
+        
+        return len(seen)
