dc983b4d0c32fffad956b0db4e85891a55a4ef9d
kiranmayee09 <kiranmayee0919@gmail.com>
2026-09-16 00:14:16 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (13.36%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.py
@@ -0,0 +1,7 @@
+class Solution:
+    def titleToNumber(self, columnTitle: str) -> int:
+        ans = 0 
+        for ch in columnTitle: 
+            value = ord(ch) - ord('A') + 1 
+            ans = ans * 26 + value 
+        return ans
